bcbcd6a71501f8be4d6ee524b6922bf1e8de16a6
MalinBrag <malinbragazzi@gmail.com>
2024-08-25 16:55:49 +0000

    adding routes and start components

--- malins-recipe-box/src/app/app.routes.ts
+++ malins-recipe-box/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'header', component: HeaderComponent }
+];
