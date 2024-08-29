c8eba3e26885906b43ef36297eed6aedd4811251
MalinBrag <malinbragazzi@gmail.com>
2024-08-29 21:04:02 +0200

    adding filter layout and footer component

--- malins-recipe-box/src/app/app.routes.ts
+++ malins-recipe-box/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { FilterFormComponent } from './components/filter-form/filter-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'header', component: HeaderComponent },
     { path: 'filter', component: FilterComponent },
+    { path: 'filter-form', component: FilterFormComponent },
+    { path: 'footer', component: FooterComponent },
 
 ];
