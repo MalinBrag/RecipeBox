0206535ae5f7490b97454d9d0fbdcc3b9b6988b5
MalinBrag <malinbragazzi@gmail.com>
2024-09-30 12:15:27 +0200

    changing URL and dir path

--- malins-recipe-box/src/environments/environment.prod.ts
+++ malins-recipe-box/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://recipeapi-production-5ff9.up.railway.app'
+    apiUrl: 'https://backend-production-daa4.up.railway.app'
   };
