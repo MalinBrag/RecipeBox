68f5084ab46812d33708ff9f99cbf3d96807988f
MalinBrag <malinbragazzi@gmail.com>
2024-09-30 15:22:27 +0200

    config

--- RecipeAPI/config/cors.php
+++ RecipeAPI/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://frontend-production-8e53.up.railway.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
